291c378cd428a4f204bbe094501384c696fe32c9
mona-medhat <33934895+mona-medhat@users.noreply.github.com>
2018-05-03 19:12:02 +0200

    Update models.py

--- posts/models.py
+++ posts/models.py
@@ -26,3 +26,20 @@ class Post(models.Model):
     #     #model=models.Post
     #     likes.append('Mona')
     #     likescount+=1
+class Friend(models.Model):
+    users=models.ManyToManyField(User)
+    current_user=models.ForeignKey(User , on_delete=models.CASCADE,related_name='owner',null=True)
+
+    @classmethod
+    def make_friend(cls,current_user,new_friend):
+        friend, created =cls.objects.get_or_create(
+            current_user=current_user
+        )
+        friend.users.add(new_friend)
+
+    @classmethod
+    def lose_friend(cls,current_user,new_friend):
+        friend, created =cls.objects.get_or_create(
+            current_user=current_user
+        )
+        friend.users.remove(new_friend)
